b74b16ad9f4c0ebd8503e12fa599ddb2225e0440
hsjeirkslf-ops <hsjeirksldof@gmail.com>
2026-04-28 19:47:14 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,8 @@ import sys
 from bot.heartbeat import Heartbeat
 from bot.dashboard.server import start_dashboard
 from bot.utils.logger import get_logger
+from bot.aggressive_logic import AggressiveAgent
+
 
 log = get_logger(__name__)
 
@@ -22,6 +24,7 @@ def main():
     log.info("Press Ctrl+C to stop")
 
     heartbeat = Heartbeat()
+    agent = AggressiveAgent(heartbeat)
 
     async def run_all():
         # Start dashboard server (non-blocking)
